dafe8de831172158869532c9089ba3a83cf4482b
Benedict Lee <Benflop@users.noreply.github.com>
2022-06-22 14:16:09 +0800

    Added Chrome Driver

--- src/test/Selenium/selenium/src/test/java/com/test/selenium/AppTest.java
+++ src/test/Selenium/selenium/src/test/java/com/test/selenium/AppTest.java
@@ -6,15 +6,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 // import java.time.Duration;
 
 public class AppTest {
+    public WebDriver driver;
 
-    public static WebDriver driver;
+    @Test
+    public void eightComponents() {
 
-    public static void main(String[] args) {
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "../../chromedriver");
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
 
