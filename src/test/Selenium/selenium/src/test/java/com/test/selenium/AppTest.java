42d2864319557e5c6e39ee06fcec4237bec92403
Benedict Lee <Benflop@users.noreply.github.com>
2022-06-22 14:29:42 +0800

    Add Static to main

--- src/test/Selenium/selenium/src/test/java/com/test/selenium/AppTest.java
+++ src/test/Selenium/selenium/src/test/java/com/test/selenium/AppTest.java
@@ -2,7 +2,7 @@ package com.test.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "../../chromedriver");
 
         WebDriverManager.chromedriver().setup();
