9a2116ba6d537a69ff0cf4576e810b4fa8fdce0b
Benedict Lee <Benflop@users.noreply.github.com>
2022-06-29 14:41:34 +0800

    Update SeleniumTest.java

--- src/test/Selenium/selenium/src/test/java/SeleniumTest.java
+++ src/test/Selenium/selenium/src/test/java/SeleniumTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -7,9 +8,11 @@ public class SeleniumTest {
 
   @Test
   public void chromeSession() {
-    System.setProperty("webdriver.chrome.driver", "chromedriver");
+    // System.setProperty("webdriver.chrome.driver", "chromedriver");
+    // WebDriver driver = new ChromeDriver();
 
-    WebDriver driver = new ChromeDriver();
+    WebDriverManager.chromedriver().setup();
+    ChromeDriver driver = new ChromeDriver();
 
     // Launch Website
     driver.navigate().to("http://www.google.com/");
