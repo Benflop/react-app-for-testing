018b8887c32d14d14185c5b54d0fbf3330ef09ea
Benedict Lee <Benflop@users.noreply.github.com>
2022-06-30 20:21:26 +0800

    Update SeleniumTest.java

--- src/test/Selenium/selenium/src/test/java/SeleniumTest.java
+++ src/test/Selenium/selenium/src/test/java/SeleniumTest.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -10,8 +12,13 @@ public class SeleniumTest {
 
     System.setProperty("webdriver.chrome.whitelistedIps", "");
 
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--headless");
+    options.addArguments("--disable-dev-shm-usage");
+
     WebDriverManager.chromedriver().setup();
-    ChromeDriver driver = new ChromeDriver();
+    ChromeDriver driver = new ChromeDriver(options);
 
     // Launch Website
     driver.navigate().to("http://www.google.com/");
