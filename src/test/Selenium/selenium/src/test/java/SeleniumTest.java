e2404d7f91412a31e191654d2170fedd2831c25a
Benedict Lee <Benflop@users.noreply.github.com>
2022-07-02 15:32:07 +0800

    Update SeleniumTest.java

--- src/test/Selenium/selenium/src/test/java/SeleniumTest.java
+++ src/test/Selenium/selenium/src/test/java/SeleniumTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
@@ -17,8 +18,11 @@ public class SeleniumTest {
     // options.addArguments("--headless");
     // options.addArguments("--disable-dev-shm-usage");
 
+    FirefoxOptions options = new FirefoxOptions();
+    options.addArguments("--headless");
+
     // WebDriverManager.chromedriver().setup();
-    FirefoxDriver driver = new FirefoxDriver();
+    FirefoxDriver driver = new FirefoxDriver(options);
 
     // Launch Website
     driver.navigate().to("http://www.google.com/");
