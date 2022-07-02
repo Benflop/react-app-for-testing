0153ae69237f1ad89ed8398ce85733df8bfcc9d3
Benedict Lee <Benflop@users.noreply.github.com>
2022-07-02 15:49:03 +0800

    Update SeleniumTest.java

--- src/test/Selenium/selenium/src/test/java/SeleniumTest.java
+++ src/test/Selenium/selenium/src/test/java/SeleniumTest.java
@@ -1,23 +1,12 @@
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class SeleniumTest {
 
   @Test
-  public void chromeSession() {
-    // System.setProperty("webdriver.chrome.whitelistedIps", "");
-
-    // ChromeOptions options = new ChromeOptions();
-    // options.addArguments("--no-sandbox");
-    // options.addArguments("--headless");
-    // options.addArguments("--disable-dev-shm-usage");
-
+  public void firefoxSession() {
     FirefoxOptions options = new FirefoxOptions();
     options.addArguments("--headless");
 
@@ -30,6 +19,7 @@ public class SeleniumTest {
     String expectedTitle = "Google";
     String originalTitle = driver.getTitle();
     Assert.assertEquals(originalTitle, expectedTitle);
+    System.out.println("Assertion Successful");
 
     driver.quit();
   }
