419efe302e22f0c2d4b542c134a40cdb96a1abe8
Benedict Lee <Benflop@users.noreply.github.com>
2022-07-17 08:31:47 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Check Homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("MovieLand");
   expect(linkElement).toBeInTheDocument();
 });
